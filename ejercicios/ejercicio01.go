4ec9a5d9f88fd6783b89366fc2bec7b86b0f3465
guisilfernandez <guisilfernandez@gmail.com>
2023-07-05 12:36:39 -0600

    Primer ejercicio

---
+++ ejercicios/ejercicio01.go
@@ -0,0 +1,17 @@
+package ejercicios
+
+import (
+	"strconv"
+)
+
+func ResultadosEjercicio01(valor string) (int, string) {
+	numero, err := strconv.Atoi(valor)
+	if err != nil {
+		return 0, "Hubo un error"
+	}
+	if numero > 100 {
+		return numero, "Es mayor que 100"
+	} else {
+		return numero, "Es menor que 100"
+	}
+}
--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
-	"github-cursos/godesdecero/variables"
+	"github-cursos/godesdecero/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
+	//estado, texto := variables.ConviertoaTexto(1588)
+	//fmt.Println(estado)
+	//fmt.Println(texto)
+
+	/*if os := runtime.GOOS; os == "Linux." || os == "OS X." || os == "darwin" {
+		fmt.Println("Esto no es Windows, esto es", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}*/
+
+	valor, texto := ejercicios.ResultadosEjercicio01("1985")
+	fmt.Println("Enviado", valor)
 	fmt.Println(texto)
 }
